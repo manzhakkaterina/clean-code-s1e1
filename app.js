cfa006c11d25fd2f2d6fe209ffa9091f007d376f
Katerina Manzhak <59136296+manzhakkaterina@users.noreply.github.com>
2023-04-24 22:54:14 +0300

    refactor: update js according to BEM to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="button delete";
     deleteButtonImg.src="./remove.svg";
     deleteButtonImg.className="img-remove";
+    deleteButtonImg.alt="";
     deleteButton.appendChild(deleteButtonImg);
 
 
